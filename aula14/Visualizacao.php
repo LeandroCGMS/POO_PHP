6d9d0d1426573e434bc71ff32f7583870f23f0ba
leandrocgms2 <leandrocgms2@leandrocgms2-B85M-D3PH>
2018-07-03 15:55:08 -0400

    Aulas 14 e 15.

---
+++ aula14/Visualizacao.php
@@ -0,0 +1,39 @@
+<?php
+require_once 'Gafanhoto.php';
+require_once 'Video.php';
+class Visualizacao {
+    private $espectador;
+    private $filme;
+    function __construct($espectador, $filme) {
+        $this->espectador = $espectador;
+        $this->filme = $filme;
+    }
+
+    public function avaliar() {
+        echo "<p>".$this->espectador->getNome()." achou muito bom</p>";
+    }
+    public function avaliarNota($nota) {
+        echo "<p>".$this->espectador->getNome()." deu nota ".$nota."</p>";
+    }
+    public function avaliarPorcentagem($percent) {
+        echo "<p>".$this->espectador->getNome()." acha que de 0 a 100 por cento"
+                . " esse vídeo é ".$percent."%</p>";
+    }
+    function getEspectador() {
+        return $this->espectador;
+    }
+
+    function getFilme() {
+        return $this->filme;
+    }
+
+    function setEspectador($espectador) {
+        $this->espectador = $espectador;
+    }
+
+    function setFilme($filme) {
+        $this->filme = $filme;
+    }
+
+
+}
---
+++ aula14/index.php
@@ -0,0 +1,36 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+    <head>
+        <meta charset="UTF-8">
+        <title>Aula 14</title>
+    </head>
+    <body>
+        <pre>
+        <?php
+            require_once 'Video.php';
+            require_once 'Gafanhoto.php';
+            require_once 'Visualizacao.php';
+            $v[0] = new Video("Aula 1 de POO");
+            $v[1] = new Video("Aula 12 de PHP");
+            $v[2] = new Video("Aula 12 de HTML5");
+            
+            $g = new Gafanhoto("Jubileu", 22, 'M', "juba");
+            $g1 = new Gafanhoto("Creuza", 12, 'F', "creuzita");
+            
+            $vi = new Visualizacao($g, $v[0]);
+            $vi->avaliar();
+            $vi->avaliarNota(9);
+            $vi->avaliarPorcentagem(90);
+            
+            $vi2 = new Visualizacao($g1, $v[1]);
+            $vi2->avaliar();
+            $vi2->avaliarNota(10);
+            $vi2->avaliarPorcentagem(100);
+            
+            print_r($v);
+            print_r($g);
+            print_r($g1);
+        ?>
+        </pre>
+    </body>
+</html>
